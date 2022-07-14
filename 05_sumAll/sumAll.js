7100e397d5e2517f10acf4ab4c01b43798a88986
alexeiMikhail <leleuxc@gmail.com>
2022-07-14 15:32:08 -0500

    pass first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    sum = 0;
+    for (x; x <= y; x++){
+        sum += x;
+    }
+    return sum;
 };
 
 // Do not edit below this line
