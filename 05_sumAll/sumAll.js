7d3da73b924dd0cfdb7c7406453b9e21717c1bcc
alexeiMikhail <leleuxc@gmail.com>
2022-07-14 15:43:15 -0500

    Pass 5/6 tests in suite

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(x,y) {
     sum = 0;
-    if (x % 1 != 0 || y % 1 != 0 || x < 0 || y < 0) {
+    if (x % 1 != 0 || y % 1 != 0 || x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) {
         return "ERROR";
     }
     for (x; x <= y; x++){
