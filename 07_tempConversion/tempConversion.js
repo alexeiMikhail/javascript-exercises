0f6102ae330d850c4960ba28162dde1f3b6f38c0
alexeiMikhail <leleuxc@gmail.com>
2022-07-14 16:03:41 -0500

    Pass all tests in suite

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  var tempC = (tempF - 32) / 1.8;
+  return parseFloat(tempC.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  var tempF = tempC * 1.8 + 32;
+  return parseFloat(tempF.toFixed(1));
 };
 
 // Do not edit below this line
