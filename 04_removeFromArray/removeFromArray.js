24fc6b3145d4fc372f64b9e159c256d205db27c9
alexeiMikhail <leleuxc@gmail.com>
2022-07-14 13:32:02 -0500

    Pass first test in suite

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, toDelete) {
+    var newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (toDelete != arr[i]){
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
